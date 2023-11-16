defae0212ce248351751b66f4bdf24ceba16e7fc
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 <harsha27191@gmail.com>
2023-11-16 18:43:55 +0530

    no to number

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def fl_finetune(
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type=“nf4”,
+        bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.float16,
         )
     
